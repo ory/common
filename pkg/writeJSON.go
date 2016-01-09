930cc805232909c38f2e68310b1e21f71b056d59
Aeneas Rekkas <aeneas.rekkas@serlo.org>
2016-01-09 15:21:19 +0100

    pkg: added support for indent json

--- pkg/writeJSON.go
+++ pkg/writeJSON.go
@@ -5,25 +5,40 @@ import (
 	"net/http"
 )
 
-func WriteJSON(rw http.ResponseWriter, data interface{}) {
-	writeJSON(rw, data, http.StatusOK)
-}
-
-func WriteCreatedJSON(rw http.ResponseWriter, url string, data interface{}) {
-	rw.Header().Add("Location", url)
-	writeJSON(rw, data, http.StatusCreated)
-}
-
-func writeJSON(rw http.ResponseWriter, data interface{}, code int) {
-	js, err := json.Marshal(data)
-
+func WriteIndentJSON(rw http.ResponseWriter, data interface{}) {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	writeJSON(rw, js, http.StatusOK)
+}
+
+func WriteJSON(rw http.ResponseWriter, data interface{}) {
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writeJSON(rw, js, http.StatusOK)
+}
+
+func WriteCreatedJSON(rw http.ResponseWriter, url string, data interface{}) {
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Location", url)
+	writeJSON(rw, js, http.StatusCreated)
+}
+
+func writeJSON(rw http.ResponseWriter, js []byte, code int) {
+
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(code)
 	rw.Write(js)
-
 }
